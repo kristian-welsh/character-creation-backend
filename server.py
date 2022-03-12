b775baf0d58b2a5505d3cebebc950d1011c056d3
George Harland <georgeharland12@gmail.com>
2022-03-12 18:13:56 +0000

    Docker containers connect and restructuring

--- server.py
+++ server.py
@@ -2,8 +2,10 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import pymongo
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
-IP = "127.0.0.1"
+# Mongo docker container IP
+IP = "172.17.0.2"
 PORT = 27017
 
 app = Flask(__name__)
@@ -17,17 +19,24 @@ print("server.py defining endpoint")
 def endpoint():
     
     print("Endpoint reached.")
-    #req_dict = request.get_json()
+    req_dict = request.get_json()
 
-    #database = CallDatabase(IP, PORT, "/app")
-    #database.connect()
-    #id = database.store_data("character_name", req_dict["name"])
-    #print(id)
-    #database.disconnect()
+    try:
+        database = CallDatabase(IP, PORT, "/app")
+        database.connect()
+        print("Connected to database")
+        print("QQQQQ Starting data query")
+        id = database.store_data("character_name", req_dict["name"])
+        print("Finishing data query here")
+        print(id)
+        database.disconnect()
+        print("Disconnected from database")
+    except:
+        print("Couldn't connect to database")
 
-    #print("responding to request")
-    #return '{ "_id": "' + str(id) + '" }'
-    return '{ "_id": "1234567890" }'
+    print("responding to request")
+    return '{ "_id": "' + str(id) + '" }'
+    #return '{ "_id": "1234567890" }'
 
 class CallDatabase:
 
@@ -35,10 +44,21 @@ class CallDatabase:
         self.host = host
         self.port = port
         self.db_name = db_name
+
     def connect(self):
         print("connecting to database")
         self.client = MongoClient(self.host, self.port)
+        self.check_connection()
         self.db = self.client.app
+        print(self.db)
+
+    def check_connection(self):
+        try:
+            # Blocks until connection established
+            self.client.admin.command('ping')
+            print("Database avaliable")
+        except ConnectionFailure:
+            print("Database not avaliable")
 
     def store_data(self, key, value):
         new_cl = self.db["character_sheets"]
